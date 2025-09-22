c14f8860ddd5dfd5d6a0f5d784bf3cf631f157ab
charmcitycode <46387493+charmcitycode@users.noreply.github.com>
2025-09-22 11:44:28 -0400

    Update mp3.sh

--- mp3.sh
+++ mp3.sh
@@ -12,3 +12,7 @@ echo ' | | | |/ _ \ \ /\ / / '_ \| |/ _ \ / _` |/ _` |/ _ \ '__|              '
 echo ' | |_| | (_) \ V  V /| | | | | (_) | (_| | (_| |  __/ |                 ' 
 echo ' |____/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|\___|_|                 '
 
+echo 'Enter the Youtube URL to download an mp3'
+echo ':' read url
+
+yt-dlp -o "/home/storage/downloads/youtube/" --x --audio-format mp3 $url 
